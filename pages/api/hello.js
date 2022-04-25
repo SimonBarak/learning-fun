73b001c94150071c6463d9fa7fc11b24ad56e171
Šimon Bařák <barak.simon.as@gmail.com>
2022-04-25 17:42:32 +0200

    push

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,54 +1,53 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-const url = "https://wavepagestorage.blob.core.windows.net/audiofiles/";
-
+const url = "https://wavepagestorage.blob.core.windows.net/learningapp/";
 export const PhraseObjs = [
   {
     text_de: "Du hast schöne Schuhe.",
-    audioUrl: `${url}d-45390.mp3`,
+    audioUrl: `${url}a.mp3`,
   },
   {
     text_de: "Das ist perfekt! Wir werden zusammen gehen!",
-    audioUrl: `${url}d-24357.mp3`,
+    audioUrl: `${url}q.mp3`,
   },
   {
     text_de: "Kann ich Ihnen helfen?",
-    audioUrl: `${url}d-36782.mp3`,
+    audioUrl: `${url}w.mp3`,
   },
   {
     text_de: "Schöne Jacke! Ich liebe gelbe Farbe.",
-    audioUrl: `${url}d-57059.mp3`,
+    audioUrl: `${url}e.mp3`,
   },
   {
     text_de: "Möchtest du tanzen?",
-    audioUrl: `${url}d-80899.mp3`,
+    audioUrl: `${url}r.mp3`,
   },
   {
     text_de: "Du weißt, dass ich dich liebe.",
-    audioUrl: `${url}d-12886.mp3`,
+    audioUrl: `${url}t.mp3`,
   },
   {
     text_de: "Sie trägt schwarze Kleidung",
-    audioUrl: `${url}d-33351.mp3`,
+    audioUrl: `${url}z.mp3`,
   },
   //  Witta
   {
     text_de: "Die Sonne scheint und es ist warm.",
-    audioUrl: `${url}d-49658.mp3`,
+    audioUrl: `${url}u.mp3`,
   },
   {
     text_de: "Du hast schöne Augen!",
-    audioUrl: `${url}d-74966.mp3`,
+    audioUrl: `${url}i.mp3`,
   },
   {
     text_de: "Willst du was essen gehen?",
-    audioUrl: `${url}d-51039.mp3`,
+    audioUrl: `${url}o.mp3`,
   },
   {
     text_de: "Magst du ein Kaffee trinken gehen?",
-    audioUrl: `${url}d-87525.mp3`,
+    audioUrl: `${url}p.mp3`,
   },
   {
     text_de: "Was hast du am Wochenende gemacht?",
-    audioUrl: `${url}d-78463.mp3`,
+    audioUrl: `${url}s.mp3`,
   },
 ];
